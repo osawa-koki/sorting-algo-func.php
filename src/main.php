f1cc82ebcd1806c4aaa9d4a8cdcb35810fd36e3d
osawa-koki <koki@osawa.dev>
2024-04-27 00:13:55 +0900

    選択ソートの実装。

--- src/main.php
+++ src/main.php
@@ -1,10 +1,15 @@
 <?php
 
 require_once 'bubbleSort.php';
+require_once 'selectionSort.php';
 
 $array = range(0, 9);
 shuffle($array);
 
 echo "Shuffled Array: ". join(', ', $array) . PHP_EOL;
 
-echo "BubbleSort". join(", ", bubbleSort($array)) . PHP_EOL;
+echo "BubbleSort: ". join(", ", bubbleSort($array)) . PHP_EOL;
+
+echo "Shuffled Array: ". join(', ', $array) . PHP_EOL;
+
+echo "SelectionSort: ". join(", ", selectionSort($array)) . PHP_EOL;
